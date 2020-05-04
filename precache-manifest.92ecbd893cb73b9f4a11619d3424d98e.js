c05203627e2becc4c00a4715d6e69645e23150de
Rakshit Shetty <rakshitshetty561998@gmail.com>
2020-05-04 21:14:34 +0530

    Updates

---
+++ precache-manifest.92ecbd893cb73b9f4a11619d3424d98e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0f20c6823d9d0c1377395185573913",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7b1fcdb2d5b71e17391b",
+    "url": "/monsters-rolodex/static/css/main.7c666418.chunk.css"
+  },
+  {
+    "revision": "f3d272b2414b2551bb61",
+    "url": "/monsters-rolodex/static/js/2.dda5f008.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.dda5f008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1fcdb2d5b71e17391b",
+    "url": "/monsters-rolodex/static/js/main.2aba81c6.chunk.js"
+  },
+  {
+    "revision": "4efe36084e04a1d0e079",
+    "url": "/monsters-rolodex/static/js/runtime-main.a7655b96.js"
+  }
+]);
